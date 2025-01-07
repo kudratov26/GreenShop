a54e53a6e263500b35c8a5419bb61b31853a2f65
lazeez <qudratov.com26@gmail.com>
2025-01-07 11:25:26 +0500

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin");
+import plugin from "tailwindcss/plugin";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -13,7 +13,6 @@ export default {
           xl: "1200px",
         },
       },
-
       colors: {
         green: {
           DEFAULT: "#46A358",
